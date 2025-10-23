ec1766e25a19397995640a1c76d851963babf52a
Winston Wijaya <wwij0922@uni.sydney.edu.au>
2025-10-23 13:58:02 +1100

    changed button remapping

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -30,20 +30,41 @@ public class Controller : MonoBehaviour
         float xRotation = 0;
         float zRotation = 0;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.J))
         {
             xRotation += trackerRotateAmount;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.I))
         {
             zRotation -= trackerRotateAmount;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.L))
         {
             xRotation -= trackerRotateAmount;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.K))
         {
+            zRotation += trackerRotateAmount;
+        }
+
+        if (Input.GetKey(KeyCode.U))
+        {
+            xRotation += trackerRotateAmount;
+            zRotation -= trackerRotateAmount;
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            xRotation -= trackerRotateAmount;
+            zRotation -= trackerRotateAmount;
+        }
+        if (Input.GetKey(KeyCode.M))
+        {
+            xRotation += trackerRotateAmount;
+            zRotation += trackerRotateAmount;
+        }
+        if (Input.GetKey(KeyCode.Period))
+        {
+            xRotation -= trackerRotateAmount;
             zRotation += trackerRotateAmount;
         }
 
@@ -69,6 +90,28 @@ public class Controller : MonoBehaviour
         {
             zRotation -= trackerRotateAmount;
         }
+
+         if (Input.GetKey(KeyCode.Q))
+        {
+            xRotation -= trackerRotateAmount;
+            zRotation += trackerRotateAmount;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            xRotation += trackerRotateAmount;
+            zRotation += trackerRotateAmount;
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            xRotation -= trackerRotateAmount;
+            zRotation -= trackerRotateAmount;
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            xRotation += trackerRotateAmount;
+            zRotation -= trackerRotateAmount;
+        }
+
 
         sunObject.Rotate(new Vector3(xRotation, 0, zRotation) * Time.deltaTime);
         sunIndicatorObject.Rotate(new Vector3(xRotation, 0, zRotation) * Time.deltaTime);
